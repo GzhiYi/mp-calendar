4a473bd4f0767c0a92d88bf8e1b809cbd41a9289
GzhiYi <gzy.guy@gmail.com>
2019-08-26 22:00:09 +0800

    demo

---
+++ calendar/calendar.js
@@ -0,0 +1,57 @@
+Component({
+  properties: {
+
+  },
+  data: {
+    pickDate: '',
+    pickDateDisplay: ''
+  },
+  ready() {
+    const self = this
+    self.setData({
+      pickDate: self.parseTime(new Date(), '{y}-{m}'),
+      pickDateDisplay: self.parseTime(new Date(), '{y}年{m}月')
+    })
+  },
+  methods: {
+    bindPickDateChange(event) {
+      console.log(event)
+      const { value } = event.detail
+      this.setData({
+        pickDate: value,
+        pickDateDisplay: this.parseTime(value, '{y}年{m}月')
+      })
+    },
+    parseTime(time, cFormat) {
+      if (arguments.length === 0) {
+        return null
+      }
+      const format = cFormat || '{y}-{m}-{d} {h}:{i}:{s}'
+      let date
+      if (typeof time === 'object') {
+        date = time
+      } else {
+        if (('' + time).length === 10) time = parseInt(time) * 1000
+        date = new Date(time)
+      }
+      const formatObj = {
+        y: date.getFullYear(),
+        m: date.getMonth() + 1,
+        d: date.getDate(),
+        h: date.getHours(),
+        i: date.getMinutes(),
+        s: date.getSeconds(),
+        a: date.getDay()
+      }
+      const timeStr = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
+        let value = formatObj[key]
+        if (key === 'a') return ['一', '二', '三', '四', '五', '六', '日'][value - 1]
+        if (result.length > 0 && value < 10) {
+          value = '0' + value
+        }
+        return value || 0
+      })
+      return timeStr
+    }
+  }
+})
